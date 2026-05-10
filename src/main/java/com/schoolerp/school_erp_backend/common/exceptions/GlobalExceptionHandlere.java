582a8163c8272da5e28e4599b0d4306d9a9c669d
Abhay433 <abhayn433@gmail.com>
2026-05-11 00:57:03 +0530

    upddated initial

---
+++ src/main/java/com/schoolerp/school_erp_backend/common/exceptions/GlobalExceptionHandlere.java
@@ -0,0 +1,5 @@
+package com.schoolerp.school_erp_backend.common.exceptions;
+
+public class GlobalExceptionHandlere {
+
+}
---
+++ src/main/java/com/schoolerp/school_erp_backend/common/response/PagedResponse.java
@@ -0,0 +1,5 @@
+package com.schoolerp.school_erp_backend.common.response;
+
+public class PagedResponse {
+
+}
