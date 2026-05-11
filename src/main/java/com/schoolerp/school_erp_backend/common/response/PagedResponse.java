15f157e2ca3c26087bebb69697be8117c4c982ee
Arishazmi4141 <arishazmi4141@gmail.com>
2026-05-11 23:42:17 +0530

    working on response packages

--- src/main/java/com/schoolerp/school_erp_backend/common/response/PagedResponse.java
+++ src/main/java/com/schoolerp/school_erp_backend/common/response/PagedResponse.java
@@ -1,5 +1,106 @@
 package com.schoolerp.school_erp_backend.common.response;
 
-public class PagedResponse {
+import org.springframework.data.domain.Page;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class PagedResponse<T> {
+
+    private boolean success;
+
+    private String message;
+
+    private List<T> content;
+
+    private int page;
+
+    private int size;
+
+    private long totalElements;
+
+    private int totalPages;
+
+    private boolean last;
+
+    private LocalDateTime timestamp;
+
+    public PagedResponse() {
+    }
+
+    public PagedResponse(
+            boolean success,
+            String message,
+            List<T> content,
+            int page,
+            int size,
+            long totalElements,
+            int totalPages,
+            boolean last,
+            LocalDateTime timestamp
+    ) {
+        this.success = success;
+        this.message = message;
+        this.content = content;
+        this.page = page;
+        this.size = size;
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+        this.last = last;
+        this.timestamp = timestamp;
+    }
+
+    public static <T> PagedResponse<T> fromPage(
+            Page<T> pageData,
+            String message
+    ) {
+
+        return new PagedResponse<>(
+                true,
+                message,
+                pageData.getContent(),
+                pageData.getNumber(),
+                pageData.getSize(),
+                pageData.getTotalElements(),
+                pageData.getTotalPages(),
+                pageData.isLast(),
+                LocalDateTime.now()
+        );
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<T> getContent() {
+        return content;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public boolean isLast() {
+        return last;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 }
