8a08c832e6d60e57dc167e0f887ba429d93e3893
Zohu0 <shaikhzohaib477@gmail.com>
2026-05-12 19:56:49 +0530

    super_admin_api

--- src/main/java/com/schoolerp/school_erp_backend/modules/auth/AuthController.java
+++ src/main/java/com/schoolerp/school_erp_backend/modules/auth/AuthController.java
@@ -18,15 +18,10 @@ public class AuthController {
 
 	@Autowired
 	private AuthService authService;
-
-	@Autowired
-	private UserRepository userRepository;
-
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
-	@Autowired
-	private SchoolRepository schoolRepository;
+
 
 	@PostMapping("/login")
 	public LoginResponseDto login(@Valid @RequestBody LoginRequestDto requestDto) {
@@ -37,28 +32,6 @@ public class AuthController {
 	@PostMapping("/register-super-admin")
 	public String registerSuperAdmin() {
 
-		if (userRepository.existsByEmail("admin@test.com")) {
-			return "Super admin already exists";
-		}
-
-		User user = new User();
-
-		SchoolEntity school = schoolRepository.findById(UUID.fromString(CommonConstants.SCHOOL_ID))
-				.orElseThrow(() -> new RuntimeException("School not found"));
-		
-		user.setSchool(school);
-		user.setFirstName("Mahima");
-		user.setLastName("Chaudhary");
-		user.setEmail("adminMahima@test.com");
-
-		user.setPassword(passwordEncoder.encode("admin123"));
-
-		user.setRole(UserRole.SUPER_ADMIN);
-
-		user.setIsActive(true);
-
-		userRepository.save(user);
-
-		return "Super admin created successfully";
+		return authService.createSuperAdmin();
 	}
 }
--- src/main/java/com/schoolerp/school_erp_backend/modules/auth/AuthService.java
+++ src/main/java/com/schoolerp/school_erp_backend/modules/auth/AuthService.java
@@ -1,10 +1,15 @@
 package com.schoolerp.school_erp_backend.modules.auth;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.schoolerp.school_erp_backend.common.constants.CommonConstants;
 import com.schoolerp.school_erp_backend.common.security.JwtTokenProvider;
+import com.schoolerp.school_erp_backend.modules.school.SchoolEntity;
+import com.schoolerp.school_erp_backend.modules.school.SchoolRepository;
 
 
 @Service
@@ -16,6 +21,8 @@ public class AuthService {
 	private PasswordEncoder passwordEncoder;
 	@Autowired
     private JwtTokenProvider jwtTokenProvider;
+	@Autowired
+	private SchoolRepository schoolRepository;
 
 
 //	public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
@@ -41,5 +48,32 @@ public class AuthService {
 		String token = jwtTokenProvider.generateToken(user);
 
 		return new LoginResponseDto("Login successful", user.getRole(),token);
+	}
+	
+	public String createSuperAdmin() {
+
+		if (userRepository.existsByEmail("admin@test.com")) {
+			return "Super admin already exists";
+		}
+
+		User user = new User();
+
+		SchoolEntity school = schoolRepository.findById(UUID.fromString(CommonConstants.SCHOOL_ID))
+				.orElseThrow(() -> new RuntimeException("School not found"));
+		
+		user.setSchool(school);
+		user.setFirstName("Mahima");
+		user.setLastName("Chaudhary");
+		user.setEmail("adminMahima@test.com");
+
+		user.setPassword(passwordEncoder.encode("admin123"));
+
+		user.setRole(UserRole.SUPER_ADMIN);
+
+		user.setIsActive(true);
+
+		userRepository.save(user);
+		
+		return "Super admin created successfully";
 	}
 }
