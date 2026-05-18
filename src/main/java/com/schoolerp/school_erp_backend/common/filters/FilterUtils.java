4cebdd872a0b73cd52c422832a68b195a0bd6654
Zohu0 <shaikhzohaib477@gmail.com>
2026-05-18 22:39:42 +0530

    change style

--- src/main/java/com/schoolerp/school_erp_backend/common/filters/FilterUtils.java
+++ src/main/java/com/schoolerp/school_erp_backend/common/filters/FilterUtils.java
@@ -7,117 +7,70 @@ import jakarta.persistence.criteria.Root;
 
 public class FilterUtils {
 
-    private FilterUtils() {
-    }
+	private FilterUtils() {
+	}
 
-    public static Predicate likeIgnoreCase(
-            CriteriaBuilder cb,
-            Root<?> root,
-            String field,
-            String value
-    ) {
+	public static Predicate likeIgnoreCase(CriteriaBuilder cb, Root<?> root, String field, String value) {
 
-        if (value == null || value.isBlank()) {
-            return null;
-        }
+		if (value == null || value.isBlank()) {
+			return null;
+		}
 
-        return cb.like(
-                cb.lower(root.get(field)),
-                "%" + value.toLowerCase() + "%"
-        );
-    }
+		return cb.like(cb.lower(root.get(field)), "%" + value.toLowerCase() + "%");
+	}
 
-    public static Predicate equal(
-            CriteriaBuilder cb,
-            Root<?> root,
-            String field,
-            Object value
-    ) {
+	public static Predicate equal(CriteriaBuilder cb, Root<?> root, String field, Object value) {
 
-        if (value == null) {
-            return null;
-        }
+		if (value == null) {
+			return null;
+		}
 
-        return cb.equal(root.get(field), value);
-    }
+		return cb.equal(root.get(field), value);
+	}
 
-    public static Predicate greaterThan(
-            CriteriaBuilder cb,
-            Root<?> root,
-            String field,
-            Comparable value
-    ) {
+	public static Predicate greaterThan(CriteriaBuilder cb, Root<?> root, String field, Comparable value) {
 
-        if (value == null) {
-            return null;
-        }
+		if (value == null) {
+			return null;
+		}
 
-        return cb.greaterThan(root.get(field), value);
-    }
+		return cb.greaterThan(root.get(field), value);
+	}
 
-    public static Predicate lessThan(
-            CriteriaBuilder cb,
-            Root<?> root,
-            String field,
-            Comparable value
-    ) {
+	public static Predicate lessThan(CriteriaBuilder cb, Root<?> root, String field, Comparable value) {
 
-        if (value == null) {
-            return null;
-        }
+		if (value == null) {
+			return null;
+		}
 
-        return cb.lessThan(root.get(field), value);
-    }
+		return cb.lessThan(root.get(field), value);
+	}
 
-    public static Predicate joinEqual(
-            CriteriaBuilder cb,
-            Root<?> root,
-            String joinField,
-            String targetField,
-            Object value
-    ) {
+	public static Predicate joinEqual(CriteriaBuilder cb, Root<?> root, String joinField, String targetField,
+			Object value) {
 
-        if (value == null) {
-            return null;
-        }
+		if (value == null) {
+			return null;
+		}
 
-        return cb.equal(
-                root.get(joinField).get(targetField),
-                value
-        );
-    }
+		return cb.equal(root.get(joinField).get(targetField), value);
+	}
 
-    public static Predicate startsWith(
-            CriteriaBuilder cb,
-            Root<?> root,
-            String field,
-            String value
-    ) {
+	public static Predicate startsWith(CriteriaBuilder cb, Root<?> root, String field, String value) {
 
-        if (value == null || value.isBlank()) {
-            return null;
-        }
+		if (value == null || value.isBlank()) {
+			return null;
+		}
 
-        return cb.like(
-                cb.lower(root.get(field)),
-                value.toLowerCase() + "%"
-        );
-    }
+		return cb.like(cb.lower(root.get(field)), value.toLowerCase() + "%");
+	}
 
-    public static Predicate endsWith(
-            CriteriaBuilder cb,
-            Root<?> root,
-            String field,
-            String value
-    ) {
+	public static Predicate endsWith(CriteriaBuilder cb, Root<?> root, String field, String value) {
 
-        if (value == null || value.isBlank()) {
-            return null;
-        }
+		if (value == null || value.isBlank()) {
+			return null;
+		}
 
-        return cb.like(
-                cb.lower(root.get(field)),
-                "%" + value.toLowerCase()
-        );
-    }
+		return cb.like(cb.lower(root.get(field)), "%" + value.toLowerCase());
+	}
 }
--- src/main/java/com/schoolerp/school_erp_backend/modules/student/StudentSpecification.java
+++ src/main/java/com/schoolerp/school_erp_backend/modules/student/StudentSpecification.java
@@ -1,6 +1,5 @@
 package com.schoolerp.school_erp_backend.modules.student;
 
-
 import org.springframework.data.jpa.domain.Specification;
 
 import com.schoolerp.school_erp_backend.common.filters.FilterUtils;
@@ -8,77 +7,41 @@ import com.schoolerp.school_erp_backend.common.filters.SpecificationBuilder;
 
 public class StudentSpecification {
 
-    private StudentSpecification() {
-    }
+	private StudentSpecification() {
+	}
 
-    public static Specification<StudentEntity> filter(
-            StudentFilterRequest request
-    ) {
+	public static Specification<StudentEntity> filter(StudentFilterRequest request) {
 
-        return new SpecificationBuilder<StudentEntity>()
+		return new SpecificationBuilder<StudentEntity>()
 
-                .with(firstNameLike(request.getFirstName()))
+				.with(firstNameLike(request.getFirstName()))
 
-                .with(lastNameLike(request.getLastName()))
+				.with(lastNameLike(request.getLastName()))
 
-                .with(admissionNoEqual(request.getAdmissionNo()))
+				.with(admissionNoEqual(request.getAdmissionNo()))
 
-                .with(classIdEqual(request.getClassId()))
+				.with(classIdEqual(request.getClassId()))
 
-                .build();
-    }
+				.build();
+	}
 
-    public static Specification<StudentEntity> firstNameLike(
-            String firstName
-    ) {
+	public static Specification<StudentEntity> firstNameLike(String firstName) {
 
-        return (root, query, cb) ->
-                FilterUtils.likeIgnoreCase(
-                        cb,
-                        root,
-                        "firstName",
-                        firstName
-                );
-    }
+		return (root, query, cb) -> FilterUtils.likeIgnoreCase(cb, root, "firstName", firstName);
+	}
 
-    public static Specification<StudentEntity> lastNameLike(
-            String lastName
-    ) {
+	public static Specification<StudentEntity> lastNameLike(String lastName) {
 
-        return (root, query, cb) ->
-                FilterUtils.likeIgnoreCase(
-                        cb,
-                        root,
-                        "lastName",
-                        lastName
-                );
-    }
+		return (root, query, cb) -> FilterUtils.likeIgnoreCase(cb, root, "lastName", lastName);
+	}
 
-    public static Specification<StudentEntity> admissionNoEqual(
-            String admissionNo
-    ) {
+	public static Specification<StudentEntity> admissionNoEqual(String admissionNo) {
 
-        return (root, query, cb) ->
-                FilterUtils.equal(
-                        cb,
-                        root,
-                        "admissionNo",
-                        admissionNo
-                );
-    }
+		return (root, query, cb) -> FilterUtils.equal(cb, root, "admissionNo", admissionNo);
+	}
 
+	public static Specification<StudentEntity> classIdEqual(Long classId) {
 
-    public static Specification<StudentEntity> classIdEqual(
-            Long classId
-    ) {
-
-        return (root, query, cb) ->
-                FilterUtils.joinEqual(
-                        cb,
-                        root,
-                        "schoolClass",
-                        "id",
-                        classId
-                );
-    }
+		return (root, query, cb) -> FilterUtils.joinEqual(cb, root, "schoolClass", "id", classId);
+	}
 }
