3b43a0ddbc3984d82000d63970d01f830ab264a4
Zohu0 <shaikhzohaib477@gmail.com>
2026-05-20 04:22:44 +0530

    add CreateTeacherDto

---
+++ src/main/java/com/schoolerp/school_erp_backend/modules/teacher/CreateTeacherDto.java
@@ -0,0 +1,87 @@
+package com.schoolerp.school_erp_backend.modules.teacher;
+
+import java.time.LocalDate;
+
+public class CreateTeacherDto {
+	
+	
+    private String firstName;
+
+    private String lastName; 
+
+    private String email;
+
+    private String password;
+
+    private String employeeCode;
+
+    private String qualification;
+
+    private LocalDate joiningDate;
+
+    private String userId;
+
+    public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public LocalDate getJoiningDate() {
+        return joiningDate;
+    }
+
+    public void setJoiningDate(LocalDate joiningDate) {
+        this.joiningDate = joiningDate;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
