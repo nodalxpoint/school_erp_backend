000b6639a5d06d353a012c4750c01fe627880c3a
Zohaib <zohaib@example.com>
2026-07-22 16:48:17 +0530

    feat: add ExamMarksController to handle exam marks retrieval and updates

--- src/main/java/com/schoolerp/school_erp_backend/modules/exam/ExamMarksController.java
+++ src/main/java/com/schoolerp/school_erp_backend/modules/exam/ExamMarksController.java
@@ -1,6 +1,5 @@
 package com.schoolerp.school_erp_backend.modules.exam;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.slf4j.Logger;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.schoolerp.school_erp_backend.common.response.ApiResponse;
 import com.schoolerp.school_erp_backend.common.response.PagedResponse;
 import com.schoolerp.school_erp_backend.common.security.CustomUserDetails;
 
@@ -33,13 +33,13 @@ public class ExamMarksController {
 	}
 
 	@PostMapping("/addOrUpdate")
-	public ResponseEntity<Void> addOrUpdateExamMarks(@RequestBody ExamMarksDto request
+	public ResponseEntity<ApiResponse<Void>> addOrUpdateExamMarks(@RequestBody ExamMarksDto request
 		,@AuthenticationPrincipal CustomUserDetails userDetails) {
-        UUID userId = userDetails.getId();
+		UUID userId = userDetails.getId();
 
 		LOGGER.debug("addOrUpdateExamMarks endpoint called");
-		examMarksService.addOrUpdateExamMarks(request,userId);
-		return ResponseEntity.ok().build();
+		examMarksService.addOrUpdateExamMarks(request, userId);
+		return ResponseEntity.ok(ApiResponse.success("Exam marks saved successfully", null));
 	}
 
 }
